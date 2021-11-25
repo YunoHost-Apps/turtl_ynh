19910427ef05bad1d83295d986d0da904d798e22
ericgaspar <junk.eg@free.fr>
2021-11-25 15:04:37 +0100

    Fix

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -8,6 +8,7 @@
 pkg_dependencies="postgresql postgresql-contrib"
 
 nodejs_version=12
+version_commit=0e59ee0a47fc5937600999d14ceb1aca6392aec8
 
 #=================================================
 # PERSONAL HELPERS
