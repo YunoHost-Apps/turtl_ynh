9e09f44890f0c3d497f6949f011129edde3abc69
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2023-01-26 18:23:06 +0100

    Update _common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -7,7 +7,7 @@
 # dependencies used by the app
 pkg_dependencies="postgresql postgresql-contrib"
 
-nodejs_version=12
+nodejs_version=14
 version_commit=0e59ee0a47fc5937600999d14ceb1aca6392aec8
 
 #=================================================
