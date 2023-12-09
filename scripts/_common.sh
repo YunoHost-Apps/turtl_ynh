3c2c673ef015c76d0068b636c4db2b48643f58eb
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2023-12-09 20:50:53 +0100

    v2

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,9 +4,6 @@
 # COMMON VARIABLES
 #=================================================
 
-# dependencies used by the app
-#REMOVEME? pkg_dependencies="postgresql postgresql-contrib"
-
 nodejs_version=14
 version_commit=0e59ee0a47fc5937600999d14ceb1aca6392aec8
 
